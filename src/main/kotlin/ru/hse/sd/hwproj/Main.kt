0fcf56c1f04baa6eded862a6fffe9469b47bee7f
GlebSolovev <lionyoung@mail.ru>
2022-06-19 15:34:36 +0300

    Fix storage directory path not existing

--- src/main/kotlin/ru/hse/sd/hwproj/Main.kt
+++ src/main/kotlin/ru/hse/sd/hwproj/Main.kt
@@ -4,9 +4,15 @@ import ru.hse.sd.hwproj.interactor.EntityGateway
 import ru.hse.sd.hwproj.interactor.Interactor
 import ru.hse.sd.hwproj.io.createServer
 import ru.hse.sd.hwproj.storage.sqlite.SQLiteStorage
+import java.io.File
+import java.nio.file.Paths
+
+const val STORAGE_DIRECTORY_PATH = "src/main/resources/db"
+const val STORAGE_FILE = "test.db"
 
 fun main() {
-    val storage = SQLiteStorage("src/main/resources/db/test.db")
+    File(STORAGE_DIRECTORY_PATH).mkdirs()
+    val storage = SQLiteStorage(Paths.get(STORAGE_DIRECTORY_PATH, STORAGE_FILE).toString())
 
     val entityGateway = EntityGateway(storage)
     val interactor = Interactor(entityGateway)
